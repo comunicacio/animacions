45275cbbf055788d4ccaa203b1b2107af6f2c52a
orodriguez <orodriguez@sonicon.net>
2018-03-05 11:58:19 +0100

    Roll-over correction

--- animacions/menu.js
+++ animacions/menu.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-	$(".btn").click(function() {
-		$(".btn").removeClass("active");
-		if(!$(this).hasClass("disable")) $(this).toggleClass("active");
-	});
+jQuery(document).ready(function() {
+  jQuery(".btn").click(function() {
+
+    jQuery(this).toggleClass("active");
+  });
  
 
 
-  if(jQuery(window).width() >= 1024) {
+  if(jQuery(window).width() >= 800) {
     jQuery('.btn').click(false);
 
     jQuery('.btn').hover(function() {
-      $(".btn").removeClass("active");
-      if(!$(this).hasClass("disable")) $(this).toggleClass("active");
+
+      jQuery(this).addClass("active");
     });
     jQuery('.btn').mouseleave(function() {
-      $(".btn").removeClass("active");
-      $(this).removeClass("active");
+
+      jQuery(this).removeClass("active");
     });
   }
 });
