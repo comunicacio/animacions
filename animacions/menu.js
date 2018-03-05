47377acdaebcabba776aeb65ef9397d33e40c6a5
orodriguez <orodriguez@sonicon.net>
2018-03-05 12:19:04 +0100

    Click Issue

--- animacions/menu.js
+++ animacions/menu.js
@@ -1,21 +1,19 @@
 jQuery(document).ready(function() {
-  jQuery(".btn").click(function() {
+	jQuery(".btn").click(function() {
+		jQuery(this).toggleClass("active");
+	});
 
-    jQuery(this).toggleClass("active");
-  });
- 
+	if(jQuery(window).width() >= 800) {
+    
+		jQuery('.btn').hover(function() {
+			jQuery(this).addClass("active");
+		});
 
-
-  if(jQuery(window).width() >= 800) {
-    jQuery('.btn').click(false);
-
-    jQuery('.btn').hover(function() {
-
-      jQuery(this).addClass("active");
-    });
-    jQuery('.btn').mouseleave(function() {
-
-      jQuery(this).removeClass("active");
-    });
-  }
+		jQuery('.btn').mouseleave(function() {
+			jQuery(this).removeClass("active");
+		});
+	}
+  
+	jQuery('.btn.disable').click(false);
+	
 });
